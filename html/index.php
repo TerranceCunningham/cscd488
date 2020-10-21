22ee024de65b453b236a42e34c76fe67286118d7
rdeskins <rdeskins17@gmail.com>
2020-10-20 17:07:05 -0700

    combine tk-build-env and unit tests

--- html/index.php
+++ html/index.php
@@ -1,1 +1,9 @@
-<h1>this is the main page</h1>
+<?php
+    require "../includes/mainHeader.php";
+?>
+
+    <h1>this is the main page</h1>
+    
+<?php
+    require "../includes/mainFooter.php";
+?>
