3a02d664e6f7ac5b160d9960857d06701789279a
Terrance <terrancejcunningham@gmail.com>
2020-10-26 20:49:53 -0700

    created front page beta, still fixing header

--- index.php
+++ index.php
@@ -1,1 +1,51 @@
-<html><head><title>PHP Info</title></head><body><?php phpinfo(); ?></body></html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="styles/styles.css">
+        <title>PHP Info</title>
+    </head>
+    <body>
+        <div class="wrapper">
+            <div class="header"> 
+                <div class="headerContainer">
+                        <div class="logo">
+                            <a href="index.php"><img src="images/logo.png" /></a>
+                        </div><!-- end logo class-->
+                        <div class=loginAndRegister>
+                                <div class="login">
+                                    <a href="https://www.google.com/">
+                                        <input type="button" class="button" value="Login">
+                                        <input type="button" class="button" value="Register">
+                                    </a>
+                                </div><!-- end login class-->
+                        </div><!--end loginAndRegister class-->          
+                </div><!--end headerContainer class-->        
+            </div><!--End header class-->
+
+            <div class="navbar">
+                <div class="navbarSelection">
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="about.php">About</a></li>
+                        <li><a href="gamePlay.php">GamePlay</a></li>
+                        <li><a href="contact.php">Contact</a></li>
+                    </ul>
+                </div><!--End navbarSelection class-->
+                <div class= "socialIcons">
+                    <a href="#" class="fa fa-facebook"></a>
+                    <a href="#" class="fa fa-twitter"></a>
+                    <a href="#" class="fa fa-linkedin"></a>
+                </div><!--End socialIcons class-->
+            </div><!--End navbar class-->
+            
+            <div class="container" >
+                <img src="images/FPslideshow_new1.jpg" />
+            </div><!--End container class-->
+            
+            <div class="footer">
+                <p>©copyright 2020 - Terrance Cunningham | Robin Deskins | Tony Kolste | Steven Zuelke  - Web Designers</p>
+            </div><!--End footer class-->
+        </div><!--End Wrapper class-->
+    </body>
+</html>
